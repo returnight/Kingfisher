ed939a298daf46e86baf465b229063cdfef1cf31
yueb <yueb@2345.com>
2016-06-24 11:57:32 +0800

    格式化

--- Sources/ImageTransition.swift
+++ Sources/ImageTransition.swift
@@ -26,98 +26,98 @@
 
 #if os(OSX)
 // Not implemented for OSX and watchOS yet.
-    
-import AppKit
-    
-public enum ImageTransition {
-    case None
-    var duration: NSTimeInterval {
-        return 0
-    }
-}
+
+	import AppKit
+
+	public enum ImageTransition {
+		case None
+		var duration: NSTimeInterval {
+			return 0
+		}
+	}
 
 #elseif os(watchOS)
-import UIKit
-public enum ImageTransition {
-    case None
-    var duration: NSTimeInterval {
-        return 0
-    }
-}
+	import UIKit
+	public enum ImageTransition {
+		case None
+		var duration: NSTimeInterval {
+			return 0
+		}
+	}
 #else
-import UIKit
+	import UIKit
 
-/**
-Transition effect to use when an image downloaded and set by `UIImageView` extension API in Kingfisher.
-You can assign an enum value with transition duration as an item in `KingfisherOptionsInfo` 
-to enable the animation transition.
+	/**
+	 Transition effect to use when an image downloaded and set by `UIImageView` extension API in Kingfisher.
+	 You can assign an enum value with transition duration as an item in `KingfisherOptionsInfo`
+	 to enable the animation transition.
 
-Apple's UIViewAnimationOptions is used under the hood.
-For custom transition, you should specified your own transition options, animations and 
-comletion handler as well.
+	 Apple's UIViewAnimationOptions is used under the hood.
+	 For custom transition, you should specified your own transition options, animations and
+	 comletion handler as well.
 
-- None:           No animation transistion.
-- Fade:           Fade in the loaded image.
-- FlipFromLeft:   Flip from left transition.
-- FlipFromRight:  Flip from right transition.
-- FlipFromTop:    Flip from top transition.
-- FlipFromBottom: Flip from bottom transition.
-- Custom:         Custom transition.
-*/
-public enum ImageTransition {
-    case None
-    case Fade(NSTimeInterval)
+	 - None:           No animation transistion.
+	 - Fade:           Fade in the loaded image.
+	 - FlipFromLeft:   Flip from left transition.
+	 - FlipFromRight:  Flip from right transition.
+	 - FlipFromTop:    Flip from top transition.
+	 - FlipFromBottom: Flip from bottom transition.
+	 - Custom:         Custom transition.
+	 */
+	public enum ImageTransition {
+		case None
+		case Fade(NSTimeInterval)
 
-    case FlipFromLeft(NSTimeInterval)
-    case FlipFromRight(NSTimeInterval)
-    case FlipFromTop(NSTimeInterval)
-    case FlipFromBottom(NSTimeInterval)
-    
-    case Custom(duration: NSTimeInterval,
-                 options: UIViewAnimationOptions,
-              animations: ((UIImageView, UIImage) -> Void)?,
-              completion: ((Bool) -> Void)?)
-    
-    var duration: NSTimeInterval {
-        switch self {
-        case .None:                          return 0
-        case .Fade(let duration):            return duration
-            
-        case .FlipFromLeft(let duration):    return duration
-        case .FlipFromRight(let duration):   return duration
-        case .FlipFromTop(let duration):     return duration
-        case .FlipFromBottom(let duration):  return duration
-            
-        case .Custom(let duration, _, _, _): return duration
-        }
-    }
-    
-    var animationOptions: UIViewAnimationOptions {
-        switch self {
-        case .None:                         return .TransitionNone
-        case .Fade(_):                      return .TransitionCrossDissolve
-            
-        case .FlipFromLeft(_):              return .TransitionFlipFromLeft
-        case .FlipFromRight(_):             return .TransitionFlipFromRight
-        case .FlipFromTop(_):               return .TransitionFlipFromTop
-        case .FlipFromBottom(_):            return .TransitionFlipFromBottom
-            
-        case .Custom(_, let options, _, _): return options
-        }
-    }
-    
-    var animations: ((UIImageView, UIImage) -> Void)? {
-        switch self {
-        case .Custom(_, _, let animations, _): return animations
-        default: return {$0.image = $1}
-        }
-    }
-    
-    var completion: ((Bool) -> Void)? {
-        switch self {
-        case .Custom(_, _, _, let completion): return completion
-        default: return nil
-        }
-    }
-}
+		case FlipFromLeft(NSTimeInterval)
+		case FlipFromRight(NSTimeInterval)
+		case FlipFromTop(NSTimeInterval)
+		case FlipFromBottom(NSTimeInterval)
+
+		case Custom(duration: NSTimeInterval,
+			options: UIViewAnimationOptions,
+			animations: ((UIImageView, UIImage) -> Void)?,
+			completion: ((Bool) -> Void)?)
+
+		var duration: NSTimeInterval {
+			switch self {
+			case .None: return 0
+			case .Fade(let duration): return duration
+
+			case .FlipFromLeft(let duration): return duration
+			case .FlipFromRight(let duration): return duration
+			case .FlipFromTop(let duration): return duration
+			case .FlipFromBottom(let duration): return duration
+
+			case .Custom(let duration, _, _, _): return duration
+			}
+		}
+
+		var animationOptions: UIViewAnimationOptions {
+			switch self {
+			case .None: return .TransitionNone
+			case .Fade(_): return .TransitionCrossDissolve
+
+			case .FlipFromLeft(_): return .TransitionFlipFromLeft
+			case .FlipFromRight(_): return .TransitionFlipFromRight
+			case .FlipFromTop(_): return .TransitionFlipFromTop
+			case .FlipFromBottom(_): return .TransitionFlipFromBottom
+
+			case .Custom(_, let options, _, _): return options
+			}
+		}
+
+		var animations: ((UIImageView, UIImage) -> Void)? {
+			switch self {
+			case .Custom(_, _, let animations, _): return animations
+			default: return { $0.image = $1 }
+			}
+		}
+
+		var completion: ((Bool) -> Void)? {
+			switch self {
+			case .Custom(_, _, _, let completion): return completion
+			default: return nil
+			}
+		}
+	}
 #endif
